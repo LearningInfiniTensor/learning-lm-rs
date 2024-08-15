c64bc5763c6410ca977f3efd97318c3b423b37bd
scat <suzuki1968@protonmail.com>
2024-08-15 14:55:32 +0800

    silu

--- src/operators.rs
+++ src/operators.rs
@@ -77,13 +77,17 @@ pub fn rms_norm(y: &mut Tensor<f32>, x: &Tensor<f32>, w: &Tensor<f32>, epsilon: 
 // y = sigmoid(x) * x * y
 // hint: this is an element-wise operation
 pub fn silu(y: &mut Tensor<f32>, x: &Tensor<f32>) {
-    // let len = y.size();
-    // assert!(len == x.size());
+    let len = y.size();
+    assert!(len == x.size());
 
-    // let _y = unsafe { y.data_mut() };
-    // let _x = x.data();
+    let _y = unsafe { y.data_mut() };
+    let _x = x.data();
 
-    todo!("实现 silu，这里给了一些前期准备工作的提示，你可以参考")
+    for i in 0..len {
+        let xi = _x[i];
+        let sigmoid = 1.0 / (1.0 + (-xi).exp()); // 计算 sigmoid
+        _y[i] = xi * sigmoid * _y[i]; // 计算 silu
+    }
 }
 
 // C = beta * C + alpha * A @ B^T
